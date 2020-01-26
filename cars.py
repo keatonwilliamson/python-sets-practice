aebb7948ac01bf25e778e62a69f2bdfa1c03f181
George Williamson <keatonwilliamson@Georges-MacBook-Pro.local>
2020-01-26 12:31:12 -0600

    added to set

--- cars.py
+++ cars.py
@@ -1,4 +1,13 @@
 # 1. Create an empty set named showroom.
 # 2. Add four of your favorite car model names to the set.
 showroom = {"Toyota", "Nissan", "Ford", "Honda"}
+
+# 3. Print the length of your set.
 print(len(showroom))
+
+# 4. Pick one of the items in your show room and add it to the set again.
+showroom.add("Toyota")
+print(len(showroom))
+
+
+
